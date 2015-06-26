5324eed089976c3ac27c9a5e28783a6af04daea4
Alec Bibat <alecbibat@gmail.com>
2015-06-26 13:47:48 -0600

    foreign key :posts, :users migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626190647) do
+ActiveRecord::Schema.define(version: 20150626194015) do
 
   create_table "posts", force: true do |t|
     t.string "url"
